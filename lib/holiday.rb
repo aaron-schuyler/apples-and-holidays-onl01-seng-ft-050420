a20b1058c7a9ccfe006df598d091f293a6a6cb19
Aaron Schuyler <asky28@gmail.com>
2020-05-09 23:12:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,12 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  list =[]
+  holiday.each do |key, value|
+    value.each do |holiday, values|
+      if values.containes?
+    end
+  end
 end
 
 
